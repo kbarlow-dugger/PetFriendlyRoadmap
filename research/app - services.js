dbcdcf88fc60d9a19ed327d2b00ed0a6b2c86e1d
kbarlow-dugger <108016845+kbarlow-dugger@users.noreply.github.com>
2022-10-03 22:35:22 -0400

    Add files via upload

--- research/app - services.js
+++ research/app - services.js
@@ -104,13 +104,16 @@ function initMap(business, latlng) {
     results.forEach(function(business) {
       // Create each business result div
       let businessInfo = document.createElement('table');
-      let businessName = business.name;	  
+      let businessName = business.name;
+	  let businessPrice = business.price;
+	  if (businessPrice === undefined) {businessPrice="?";}
+	  let businessRating = business.rating;		  
       let businessAddress = `${business.location.display_address[0]}, ${business.location.display_address[1]}`;
 	  let businessCat = business.categories[0].title; 
 	  let businessURL = business.url;	  
 	  let lat = business.coordinates.latitude; let long = business.coordinates.longitude;
 	  let distance = Math.round(business.distance,0);
-      businessInfo.innerHTML = `<tr><td width="350px;">${businessName} <a href=${businessURL} target="_new">(yelp)</a></td><td width="400px;">${businessAddress}</td><td width="160px;">${businessCat}</td><td width="160px;">${lat}</td><td width="160px;">${long}</td><td style="text-align:right; width:70px;">${distance}</td></tr>`; 
+      businessInfo.innerHTML = `<tr><td width="350px;">${businessName} <a href=${businessURL} target="_new">(yelp)</a></td><td width="400px;">${businessAddress}</td><td width="160px;">${businessCat}</td><td width="60px;">${businessPrice} ${businessRating}</td><td style="text-align:right; width:70px;">${distance}</td></tr>`; 
 	  
       // Create each details div
       let details = document.createElement('div');
