46372f07498cdf2f28dc2d5dafc8ed926c0c5bd3
kbarlow-dugger <108016845+kbarlow-dugger@users.noreply.github.com>
2023-05-22 17:31:56 -0400

    Add files via upload

--- research/app - parks.js
+++ research/app - parks.js
@@ -80,7 +80,7 @@ function initMap(business, latlng) {
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=parks&limit=65&radius=20000&sort_by=distance&location=${window.searchText}&term=${window.selectCat}`,
+    "url": `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=parks&radius=20000&sort_by=distance&location=${window.searchText}&term=${window.selectCat}`,
     "method": "GET",
     "headers": {
       "authorization": "Bearer Ye-91IGpkqa8Awzi7I2IaNGtfBBICvZjqIRSK-I41kv-GLafsgdSQCh5n7LRZXwKlZDD4TGCTksDCPXg94VNDqyAWREdlje_XFYdjgFXNs8bSkDS5dFmY7wRAoWyYnYx"
